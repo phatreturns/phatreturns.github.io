1c293508588fac3b524ca01bfc425d7b0642e1d0
Hilary <hilary@objectiveassociates.com.au>
2021-04-06 12:38:41 +1000

    added logic for captcha verification by backend

--- assets/scripts/submitForm.js
+++ assets/scripts/submitForm.js
@@ -22,35 +22,79 @@ $("#Contact_Form").submit(async function submitForm() {
         // do request for recaptcha token
         // response is promise with passed token
         grecaptcha.execute('6Ld3jpQaAAAAAPp0bz0rCE5ZYjYOLthv-5C7TbDO', {action: 'send_message'}).then(function(token) {
-          // add token to form
+          // check token
           if (token !== "") {
-            $('#Contact_Form').prepend('<input type="hidden" name="g-recaptcha-response" value="' + token + '">')
-            event.preventDefault(); //prevent page reload
-            console.log("getting form data");
-            const formData = {
-              clientFirstName: `${firstName.value}`,
-              clientLastName: `${lastName.value}`,
-              clientEmail: `${email.value}`,
-              clientPhone: `${phone.value}`,
-              clientMessage: `${message.value}`
+            const data = {
+              recaptchaToken: token
             };
-            console.log("running fetch");
-            fetch("https://dev-api.codeology.com.au/pickle/email/send", {
+            console.log("running recaptcha verfication");
+            fetch("https://dev-api.codeology.com.au/pickle/site/captcha", {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "https://www.phatreturns.com.au/"
               },
               redirect: "follow",
-              body: JSON.stringify(formData)
+              body: JSON.stringify(data)
             })
-              .then(response =>
-                alert("Thank you for submitting a request. We will be in touch soon!")
-              )
-              .then(response => location.reload())
-              .catch(error => console.error("Error: ", error));
+            .then(response =>
+              console.log('recaptcha ok, sending email')
+            )
+            .then(response => {
+              // send email
+              event.preventDefault(); //prevent page reload
+              console.log("getting form data");
+              const formData = {
+                clientFirstName: `${firstName.value}`,
+                clientLastName: `${lastName.value}`,
+                clientEmail: `${email.value}`,
+                clientPhone: `${phone.value}`,
+                clientMessage: `${message.value}`
+              };
+              console.log("running fetch");
+              fetch("https://dev-api.codeology.com.au/pickle/email/send", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  "Access-Control-Allow-Origin": "https://www.phatreturns.com.au/"
+                },
+                redirect: "follow",
+                body: JSON.stringify(formData)
+              })
+                .then(response2 =>
+                  alert("Thank you for submitting a request. We will be in touch soon!")
+                )
+                .then(response2 => location.reload())
+                .catch(error => console.error("Send Error: ", error));
+            })
+            .catch(error => console.error("Captcha Error: Bot detected?", error));
+            // $('#Contact_Form').prepend('<input type="hidden" name="g-recaptcha-response" value="' + token + '">')
+            // event.preventDefault(); //prevent page reload
+            // console.log("getting form data");
+            // const formData = {
+            //   clientFirstName: `${firstName.value}`,
+            //   clientLastName: `${lastName.value}`,
+            //   clientEmail: `${email.value}`,
+            //   clientPhone: `${phone.value}`,
+            //   clientMessage: `${message.value}`
+            // };
+            // console.log("running fetch");
+            // fetch("https://dev-api.codeology.com.au/pickle/email/send", {
+            //   method: "POST",
+            //   headers: {
+            //     "Content-Type": "application/json",
+            //     "Access-Control-Allow-Origin": "https://www.phatreturns.com.au/"
+            //   },
+            //   redirect: "follow",
+            //   body: JSON.stringify(formData)
+            // })
+            //   .then(response =>
+            //     alert("Thank you for submitting a request. We will be in touch soon!")
+            //   )
+            //   .then(response => location.reload())
+            //   .catch(error => console.error("Error: ", error));
           } else {
-            alert('BOT DETECTED')
+            console.log('recaptcha token not generated')
           }
         });
       })
