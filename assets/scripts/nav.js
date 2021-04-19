3552f0201e69e3f79c50097a713d31eb2e47e65d
Hilary <hilary@objectiveassociates.com.au>
2021-04-19 16:22:58 +1000

    minified css

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,15 +1,1 @@
-// custom js to change navbar to dark on scroll
-window.onscroll = function() {
-  myFunction();
-};
-
-var header = document.getElementById("nav");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("bg-dark");
-  } else {
-    header.classList.remove("bg-dark");
-  }
-}
+function myFunction(){window.pageYOffset>sticky?header.classList.add("bg-dark"):header.classList.remove("bg-dark")}window.onscroll=function(){myFunction()};var header=document.getElementById("nav"),sticky=header.offsetTop;
